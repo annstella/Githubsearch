f73279972b81ca56a095ae8fdc140f67a8bd6055
Annstella <annstellawangui@gmail.com>
2018-08-17 16:47:53 +0300

    add changes

--- src/app/User.ts
+++ src/app/User.ts
@@ -1,4 +1,4 @@
 export class User {
-    constructor(public name: string, public public_repos: number , public html_url: string, public avator_url: string, public date: Date) {
+    constructor(public name: string, public public_repos: number, public html_url: string, public avator_url: string, public date: Date) {
     }
 }
