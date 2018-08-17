7330d6ddd51775f3f379a37f94d44e3f00306758
Annstella <annstellawangui@gmail.com>
2018-08-17 14:22:29 +0300

    add changes

---
+++ src/app/User.ts
@@ -0,0 +1,4 @@
+export class User {
+    constructor(public name: string, public public_repos: number , public html_url: string, public avator_url: string, public date: Date) {
+    }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false
+  , myApi: '20cb349c106e546afe43deb42676ccc66ebf970a'
 };
 
 /*
