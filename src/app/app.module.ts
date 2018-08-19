9795516801798c4d3b5bbf22404b3ca7e9c39279
Annstella <annstellawangui@gmail.com>
2018-08-19 22:53:07 +0300

    add form-search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
+import { FormSearchComponent } from './form-search/form-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    RepositoriesComponent
+    RepositoriesComponent,
+    FormSearchComponent
   ],
   imports: [
     BrowserModule,
