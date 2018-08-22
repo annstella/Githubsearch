b4fb98ffb6631cda83fc834dca19621f818d6f0f
Annstella <annstellawangui@gmail.com>
2018-08-22 06:03:43 +0300

    add files and details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navigation/navbar.component';
 import { SearchComponent } from './user/search.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
-import {RoutingModule} from './routing/routing.module';
+// import {RoutingModule} from './routing/routing.module';
 import { RouterModule } from '@angular/router';
 import { SearchFormComponent } from './search-form/search-form.component';
 import {FormsModule} from '@angular/forms';
